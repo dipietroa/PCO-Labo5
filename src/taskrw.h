ec619f9e766583edc93f9b1e7e368db16b410fa8
Di Pietro Adrian <adrian.dipietro@heig-vd.ch>
2017-06-19 23:17:10 +0200

    add comments

--- src/taskrw.h
+++ src/taskrw.h
@@ -6,19 +6,47 @@
 #include "abstractreaderwriter.h"
 #include "waitinglogger.h"
 
+/**
+ * @brief Thread utilisant les méthodes réservées
+ *        aux lecteurs dans les classes implémentant
+ *        un des protocoles lecteur/rédacteur
+ */
 class TaskReader :public QThread{
 private:
-    AbstractReaderWriter* _protocol;
+    AbstractReaderWriter* _protocol; //classe implémentant le protocole
 public:
+    /**
+     * @brief run - boucle infinie (vérouille la lecture puis
+     *        la dévérouille continuellement)
+     */
     void run();
+
+    /**
+     * @brief Constructeur
+     * @param protocol
+     */
     TaskReader(AbstractReaderWriter* protocol);
 };
 
+/**
+ * @brief Thread utilisant les méthodes réservées
+ *        aux rédacteurs dans les classes implémentant
+ *        un des protocoles lecteur/rédacteur
+ */
 class TaskWriter :public QThread{
 private:
-    AbstractReaderWriter* _protocol;
+    AbstractReaderWriter* _protocol; //classe implémentant le protocole
 public:
+    /**
+     * @brief run - boucle infinie (vérouille l'écriture puis
+     *        la dévérouille continuellement)
+     */
     void run();
+
+    /**
+     * @brief Constructeur
+     * @param protocol
+     */
     TaskWriter(AbstractReaderWriter* protocol);
 };
 
